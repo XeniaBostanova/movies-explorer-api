03c11595f4d3f1dcf090e2353addae2a52bbbeb8
Xenia Bostanova <ksenia.bostanova@gmail.com>
2022-06-28 21:22:42 +0600

    fix: изменить порт

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useUnifiedTopology: true,
 });
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
